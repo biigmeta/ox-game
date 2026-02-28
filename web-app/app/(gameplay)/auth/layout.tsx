4d7166c7f1f6bcedcf9c9d24285531df04fc4233
Weerakorn Ch. <biigweerakorn@gmail.com>
2026-03-01 01:22:27 +0700

    redirect when logged in

--- web-app/app/(gameplay)/auth/layout.tsx
+++ web-app/app/(gameplay)/auth/layout.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
+'use client';
+import { useUserStore } from "@/stores/useUserStore";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
-  return (
-    <>
-        {children}
-    </>
-  )
+  const { data: session, status } = useSession();
+  const user = useUserStore((state) => state.user);
+  const router = useRouter();
+
+  useEffect(() => {
+    if ((session && status === "authenticated") || user) {
+      router.replace("/");
+    }
+  }, [session, status, router, user]);
+
+  return <>{children}</>;
 }
--- web-app/components/auth/SocialOAuth.tsx
+++ web-app/components/auth/SocialOAuth.tsx
@@ -1,17 +1,21 @@
 import { signIn, useSession } from "next-auth/react";
 import { FaGoogle } from "react-icons/fa6";
 import LogOutButton from "../general/LogOutButton";
+import Loading from "../general/Loading";
 export default function SocialOAuth() {
   const { data: session, status } = useSession();
+  const loginWithGoogle = async () => {
+    signIn("google");
+  };
 
-  if (status === "loading") return <div>Loading...</div>;
+  if (status === "loading") return <Loading />;
 
   if (!session) {
     return (
       <div>
         <button
           className="flex items-center justify-center gap-4 border w-full rounded-md p-2 border-[var(--border)]"
-          onClick={() => signIn("google")}
+          onClick={loginWithGoogle}
         >
           <FaGoogle /> Continue with Google
         </button>
